5b1f42461096d0b4a6d8c30cf7bb5f5499d8b299
Atanas Asprovski <atanas.asprovski@gmail.com>
2018-05-30 12:01:16 +0200

    Test commit

--- NeLutiSeCovece/NeLutiSeCovece/Form1.Designer.cs
+++ NeLutiSeCovece/NeLutiSeCovece/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(170, 81);
+            this.button1.Location = new System.Drawing.Point(78, 47);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
